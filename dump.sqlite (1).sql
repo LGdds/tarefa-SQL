d14d9144e3c38d86489879192b305dd82f074f7e
LGdds <159054428+LGdds@users.noreply.github.com>
2024-08-12 13:50:17 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE campo(
+  ID INT(6),
+  cidade VARCHAR(20) UNIQUE,
+  capacidade INT(10) NOT NULL
+  
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogador (
+  ID INT(6),
+  nome VARCHAR(30),
+  esporte VARCHAR(30)
+  
+ );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
---
+++ dump.sqlite.sql
@@ -0,0 +1,29 @@
+-- TABLE
+CREATE TABLE funcionarios (
+  nome varchar(60),
+  cpf varchar(30),
+  salario int(10),
+  setor varchar(2),
+  contratacao date(9)
+ );
+CREATE TABLE jogo (
+  fase varchar(60),
+  pontos int(30)
+ );
+CREATE TABLE produtos (
+  descricao varchar(50),
+  preco varchar(30),
+  quantidade varchar(36),
+  total varchar(5)
+ );
+CREATE TABLE restaurante (
+  nome varchar(60),
+  especialidade varchar(30)
+ );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
