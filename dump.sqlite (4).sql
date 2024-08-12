4e574a8f7161a7147cb76063ae26a2382e1574b5
LGdds <159054428+LGdds@users.noreply.github.com>
2024-08-12 16:09:42 -0300

    Add files via upload

---
+++ dump.sqlite (4).sql
@@ -0,0 +1,24 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE funcionarios (
+  ID INT(10),
+  nome VARCHAR(30),
+  cargo VARCHAR(30),
+  salario INT(20)
+  
+ );
+CREATE TABLE reservas (
+  ID INT(6),
+  nome_cliente VARCHAR(30),
+  quarto VARCHAR(30),
+  data_entrada DATE,
+  data_saida DATE
+  
+ );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
